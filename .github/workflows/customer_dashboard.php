e24e26ac5955e3bdba78af72a9fb0d0ec0cc9076
ThevinduW <120010745+thev1ndu@users.noreply.github.com>
2025-01-22 20:54:05 +0530

    Delete .github/workflows directory

--- .github/workflows/customer_dashboard.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['role']) || $_SESSION['role'] != 'customer') {
-    header("Location: login.php");
-    exit();
-}
-?>
-<h1>Welcome, Customer!</h1>
-<a href="logout.php">Logout</a>
--- .github/workflows/seller_dashboard.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['role']) || $_SESSION['role'] != 'seller') {
-    header("Location: login.php");
-    exit();
-}
-?>
-<h1>Welcome, Seller!</h1>
-<a href="logout.php">Logout</a>
